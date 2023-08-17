20d4fcb468c5f5d743ff108741e9920dc408369a
Mukul pathak <mukulpathak4@gmail.com>
2023-08-17 19:05:58 +0530

    delete btn added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -12,11 +13,9 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const db = require("./util/database");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-db.execute('Select * From products');
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'spsushila'
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'spsushila'
 });
 
 module.exports = pool.promise();
